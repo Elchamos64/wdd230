57e0a864a5e085c17375c61f17eba8b02cf419a2
Elchamos64 <ram22018@byui.edu>
2023-09-26 09:41:43 -0600

    css background change

---
+++ week02/scripts/bom.js
@@ -0,0 +1,17 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const feedback = document.querySelector('#feedback');
+
+button.addEventListener('click', () => {
+    if (input.value) {
+    console.log('clicked');
+
+    feedback.textContent = '';
+
+    } else{
+        input.focus();
+
+        feedback.textContent = "Please enter a Book and a Chapter";
+    }
+});
