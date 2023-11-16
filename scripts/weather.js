2d021c032e3ec666dc636905746e3ce8a63db4e7
Elchamos64 <ram22018@byui.edu>
2023-11-16 09:08:51 -0700

    homepage commit weather added

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7b02b55b5177a586ac1d82a615d40c15&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=15.52&lon=-88.01&appid=7b02b55b5177a586ac1d82a615d40c15&units=imperial';
 
 async function apiFetch(){
     try{
@@ -24,9 +23,8 @@ apiFetch();
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
+
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', 'weather icon');
     weatherIcon.setAttribute('loading', 'lazy');
-    captionDesc.textContent = `${desc}`;
 }
