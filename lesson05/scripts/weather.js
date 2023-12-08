ca1b058a1b8adb214108160c3b88523a3fa7fc3c
Elchamos64 <ram22018@byui.edu>
2023-12-08 14:40:43 -0700

    meta tags added

--- lesson05/scripts/weather.js
+++ lesson05/scripts/weather.js
@@ -1,32 +1,32 @@
-// const currentTemp = document.querySelector('#current-temp');
-// const weatherIcon = document.querySelector('#weather-icon');
-// const captionDesc = document.querySelector('figcaption');
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
-// const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7b02b55b5177a586ac1d82a615d40c15&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7b02b55b5177a586ac1d82a615d40c15&units=imperial';
 
-// async function apiFetch(){
-//     try{
-//         const response = await fetch(url);
-//         if (response.ok) {
-//             const data = await response.json();
-//             console.log(data);
-//             displayResults(data);
-//         } else {
-//             throw Error(await response.text());
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-// apiFetch();
+apiFetch();
 
-// function displayResults(data) {
-//     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-//     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-//     let desc = data.weather[0].description;
-//     weatherIcon.setAttribute('src', iconsrc);
-//     weatherIcon.setAttribute('alt', 'weather icon');
-//     weatherIcon.setAttribute('loading', 'lazy');
-//     captionDesc.textContent = `${desc}`;
-// }
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'weather icon');
+    weatherIcon.setAttribute('loading', 'lazy');
+    captionDesc.textContent = `${desc}`;
+}
